cb082e95f316d50a1d1b995b535553284a058ff4
Shreya Krishna <shreya7205krishna@gmail.com>
2025-08-22 21:23:27 +0530

    Add files

---
+++ src/product.jsx
@@ -0,0 +1,36 @@
+// products.js
+
+const products = [
+  {
+    id: 1,
+    name: "Jasmine Flower Bundle",
+    price: 30,
+    image: "/images/jasmine.jpg"
+  },
+  {
+    id: 2,
+    name: "Banana Chips Packet",
+    price: 50,
+    image: "/images/chips.jpg"
+  },
+  {
+    id: 3,
+    name: "Mundu (Traditional Wear)",
+    price: 250,
+    image: "/images/mundu.jpg"
+  },
+  {
+    id: 4,
+    name: "Sharkara Upperi",
+    price: 60,
+    image: "/images/sharkara.jpg"
+  },
+  {
+    id: 5,
+    name: "Kasavu Set Mundu",
+    price: 300,
+    image: "/images/kasavu.jpg"
+  }
+];
+
+export default products;
